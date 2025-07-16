54b61ef9a6a05cab63c0e7957ab25c500b1344f8
Ben McDonald <ben.mcdonald@hpe.com>
2025-07-16 14:42:33 -0700

    Add PDU scan and collect script

---
+++ pdu-part.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+run_and_wait() {
+    local cmd_string="$1"
+    echo ""
+    read -p "${cmd_string}"
+    eval "${cmd_string}"
+}
+
+set -e
+
+echo "STEP 1: Scanning for PDUs on the network..."
+run_and_wait "./magellan scan --subnet 10.254.1.0/17 -F json --include=pdus"
+
+
+echo "\n🚀 STEP 2: Collecting inventory from PDU 'x3000m0'..."
+run_and_wait "./magellan collect pdu x3000m0 --username admn --password admn -v"
