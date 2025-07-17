18b06dca00ae63e60d89adf319bd15fa45e7cd97
Ben McDonald <ben.mcdonald@hpe.com>
2025-07-17 09:57:18 -0700

    Fix newlines

--- pdu-part.sh
+++ pdu-part.sh
@@ -13,5 +13,5 @@ echo "STEP 1: Scanning for PDUs on the network..."
 run_and_wait "./magellan scan --subnet 10.254.1.0/17 -F json --include=pdus"
 
 
-echo "\n🚀 STEP 2: Collecting inventory from PDU 'x3000m0'..."
+echo -e "\n STEP 2: Collecting inventory from PDU 'x3000m0'..."
 run_and_wait "./magellan collect pdu x3000m0 --username admn --password admn -v"
