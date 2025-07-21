eedfea1568cc7c23b4c3200a88375a4ffe95cc96
Ben McDonald <ben.mcdonald@hpe.com>
2025-07-21 14:03:35 -0700

    Remove local executable from pdu

--- pdu-part.sh
+++ pdu-part.sh
@@ -10,8 +10,8 @@ run_and_wait() {
 set -e
 
 echo "STEP 1: Scanning for PDUs on the network..."
-run_and_wait "./magellan scan --subnet 10.254.1.0/17 -F json --include=pdus"
+run_and_wait "magellan scan --subnet 10.254.1.0/17 -F json --include=pdus"
 
 
 echo -e "\n STEP 2: Collecting inventory from PDU 'x3000m0'..."
-run_and_wait "./magellan collect pdu x3000m0 --username admn --password admn -v"
+run_and_wait "magellan collect pdu x3000m0 --username admn --password admn -v"
